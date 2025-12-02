dd353f3eb1fec7e275ade64aed7c439c220e60dd
adamwestman25-hash <adam.westman25@gmail.com>
2025-12-01 20:27:04 -0800

    Filtering Update for records
    Adds name + class filtering, along with better visuals

--- recordsscript.js
+++ recordsscript.js
@@ -42,24 +42,32 @@ async function loadData() {
 // Sets up all the different elements on the webpage 
 function PageSetup() {
 
-
+    // Grabbing/creating all page elements
 
     const selector = document.getElementById('DataSelector');
+    const nameSearch = document.getElementById('nameSearch'); 
+    const classFilter = document.getElementById('filterASClass'); 
+
     const grid = document.getElementById('grid');
     const datagrid = document.getElementById('datagrid');
     datagrid.style.display = 'grid';
+
+    let mainPropertiesTile = document.createElement('div2'); 
+    let inspectionTile = document.createElement('div2'); 
+
+    mainPropertiesTile.className = 'grid-square'; 
+    inspectionTile.className = 'grid-square'; 
     
-    function getBackgroundColor() {
-        const r = 93;
-        const g = 138;
-        const b = 168;
-        return `rgb(\${r},\${g},\${b})`; 
-    }; 
+    // Creating tables to hold all the collected data, and then also holding the filtered ones 
+    let fullData = [];  // All cadets
+    let classFilteredData = [];  // Cadets remaining after class filters
+    let nameFilteredData = [];  // Cadets remaining after name filters
 
     // Setup selections 
     var cadetTable = new Map(); 
+    var cadetOptionButtons = []; 
 
-    // Reformatting the retrieved data table into a map that can be more easily accessed 
+    // Reformatting the retrieved data table (rawTable) into a map that can be more easily accessed (cadetTable)
     Object.values(rawTable).forEach(index => {
         const rawName = index.hasOwnProperty("Cadet"); 
 
@@ -83,46 +91,23 @@ function PageSetup() {
                 cadetTable.set(name, cleanPropertyList)
             }
 
-        else {console.log("Could not find the name value")}
-        console.log("went through an item"); 
+        else {console.log("Could not find the name value"); }
     }); 
 
-    // Setup drop down options with cadet names as selectable values, which would then point to their data
-    for (const cadet of cadetTable.keys()) {
-
-        var cadetoption = document.createElement("option"); 
-        cadetoption.value = cadet; 
-        cadetoption.textContent = cadet;
-        
-        selector.appendChild(cadetoption); 
-
-    }
-
-    let mainPropertiesTile = document.createElement('div2'); 
-    let inspectionTile = document.createElement('div2'); 
-
-    mainPropertiesTile.className = 'grid-square'; 
-    mainPropertiesTile.style.backgroundColor = getBackgroundColor(); 
-
-    inspectionTile.className = 'grid-square'; 
-    inspectionTile.style.backgroundColor = getBackgroundColor(); 
-
-    selector.addEventListener('change', () => {
+    // Function that displays the data from a specified cadet
+    function displayData(cadet) {
 
         // Reset tiles that are potentially full of old data (or at start of session, already empty)
         mainPropertiesTile.innerHTML = ""; 
         inspectionTile.innerHTML = ""; 
+
+        mainPropertiesTile.style.color = "white"; 
+        inspectionTile.style.color = "white"; 
         grid.innerHTML = '';
         datagrid.innerHTML = '';
-
-
-        // Get new data set
-        const selectedId = selector.value;
-        if (!selectedId) return;
-
-        var cadetData = cadetTable.get(selectedId); 
-        if (!cadetData) { warn("No table entry found for " + selectedId); }
         
+        var cadetData = cadetTable.get(cadet); 
+        if (!cadetData) { console.warn("No table entry found for " + cadet); return; }
         // Start filling tiles with new data
         let htmlString = '<strong>General Data</strong><br>'; 
         let inspectionString = '<strong>Inspection Scores</strong><br>'; 
@@ -131,32 +116,128 @@ function PageSetup() {
         cadetData.forEach((propertyValue, property) => {
             if (propertyValue !== null) {
                 if (property == "Inspections") {
-                    console.log(propertyValue); 
+
                     Object.keys(propertyValue).forEach(date => {
                         inspectionString += '<br><strong>' + date + ':</strong> ' + propertyValue[date]
                     });
                     inspectionTile.innerHTML = inspectionString; 
                 } else {
-                    console.log(property); 
-                    console.log(propertyValue); 
                     htmlString += ('<br><strong>' + property + ':</strong> ' + propertyValue);
                 };
             };
         });
 
+        // Displaying the resulting data onto the page 
         mainPropertiesTile.innerHTML = htmlString; 
         datagrid.appendChild(mainPropertiesTile); 
         datagrid.appendChild(inspectionTile); 
+
+    }
+    // Function that resets and fills table with dropdown options, filtered or unfiltered
+    function fillTable() 
+    {
+        selector.innerHTML = ""; 
+        cadetOptionButtons = [];
+
+        // Creates an array that combines the two filters together, if used 
+        let filteredCadets = []; 
+
+        if (classFilteredData.length == 0 && nameFilteredData.length == 0) {filteredCadets = fullData; console.log("no filters active"); }
+        else if (classFilteredData.length >= 1 && nameFilteredData.length >= 1) 
+            {
+                console.log("Both class and name search filters combined")
+                for (const cadetIndex of classFilteredData) 
+                    {
+                        if (nameFilteredData.includes(cadetIndex)) {filteredCadets.push(cadetIndex);}
+                    }
+            }
+        else if (classFilteredData.length >= 1) {filteredCadets = classFilteredData; console.log("Only filtered via class filter") }
+        else if (nameFilteredData.length >= 1) {filteredCadets = nameFilteredData; console.log("Only filtered via name search"); }
+        else 
+        {
+            // No applicable cadets found 
+            console.log("no cadets found after filters ")
+        }
+
+
+        if (filteredCadets.length == 0) {filteredCadets.push("No cadets found");}
+
+        
+        // Goes through the list of filtered dropdown options and creates dropdown options for them 
+        for (const cadetName of filteredCadets) 
+        {
+            var cadetoption = document.createElement("option"); 
+            cadetoption.value = cadetName; 
+            cadetoption.textContent = cadetName;
+
+            // Creates buttons 
+            selector.appendChild(cadetoption); 
+            cadetOptionButtons.push(cadetoption); 
+        }
+
+        displayData(filteredCadets[0]); // Instantly displays the data of the cadet who first appears in the list 
+    }
+
+    // Putting cadet names into an array, so it can be put into a drop down menu 
+    for (const cadet of cadetTable.keys()) {
+        fullData.push(cadet)
+    }
+    fillTable() // Fill drop down menu with all cadets 
+
+    // Listens to when the cadet dropdown changes, aka when the user chooses a specific cadet they want to see 
+    selector.addEventListener('change', function(selectEvent) {
+        // Grab selected cadet 
+        const selectedId = selectEvent.target.value;
+        if (!selectedId) return;
+
+        displayData(selectedId); 
+
     });
+
+    nameSearch.addEventListener('input', function(searchEvent) {
+        let inputString = searchEvent.target.value; 
+        inputString = inputString.trim().toUpperCase(); 
+
+        nameFilteredData = []; 
+        if (inputString != "") 
+        {
+            for (const cadet of cadetTable.keys()) 
+            {
+                if (cadet.toUpperCase().includes(inputString) == true) 
+                {
+                    nameFilteredData.push(cadet); 
+                }
+            }
+
+            if (nameFilteredData.length == 0) {nameFilteredData.push("No cadets found")}
+            console.log(nameFilteredData)
+        } 
+        fillTable(); 
+    }); 
+
+    // Listens to class filter dropdown, updating the list of viewable cadets
+    classFilter.addEventListener('change', function(classEvent) {
+        
+        classFilteredData = []; 
+        if (classEvent.target.value != "None") 
+        {
+            for (const cadet of cadetTable.keys()) 
+            {
+                if (cadetTable.get(cadet).get("ASClass") == classEvent.target.value) 
+                {
+                    classFilteredData.push(cadet); 
+                }
+            }
+        }
+        fillTable(); 
+    }); 
 
     // After everything is setup, remove the loading screen and show the selector 
     document.getElementById("LoadOverlay").style.display = "none"; 
     document.getElementById("DataSelector").style.display = "block"; 
-    console.log("hide?")
-
 }; 
 
 loadData().then(
     function(success) {PageSetup()}, 
-    function(error) {warn("An error has occurred while loading data"); console.log(error); }
+    function(error) {console.warn("An error has occurred while loading data"); console.log(error); }
 )
